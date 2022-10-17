6da5f88a4f5950152eeb6c9fd0c032675ac560e4
Farhana-Jamal <farhanajamaludeen@gmail.com>
2022-10-17 23:16:44 +0530

    data read from IO pin

--- src/main.c
+++ src/main.c
@@ -1,33 +1,45 @@
-#include <driver/gpio.h>
 #include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
+#include <driver/gpio.h>
+#include<freertos/task.h>
 
 
-#define LEDPIN 2
+#define sensor GPIO_NUM_4
 
-void configureLED(void)
+#define ESP_INTR_FLAG_DEFAULT 0
+
+static int count = 0;
+
+void IRAM_ATTR isr_handler(void *arg)
 {
-    gpio_reset_pin(LEDPIN);
-    gpio_set_direction(LEDPIN,GPIO_MODE_OUTPUT);
-}
-
-void blinkLED(void)
-{
-    while (1)
-    {
-        gpio_set_level(LEDPIN,0);
-        vTaskDelay(1000/portTICK_RATE_MS);
-        gpio_set_level(LEDPIN,1);
-        vTaskDelay(1000/portTICK_RATE_MS);
+    count++;
+    printf("%d" ,count);
         
-    }
-    
 }
 
-void app_main() 
+void app_main()
 {
-    configureLED();
-    blinkLED();
-    xTaskCreate(&blinkLED,"LED_BLINK",512,NULL,5,NULL);
+    gpio_config_t io_conf =
+    {
+        .pin_bit_mask = (1ULL<<sensor),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = 1,
+        .pull_down_en = 0,
+        .intr_type = GPIO_INTR_POSEDGE
+    };
 
+    gpio_config(&io_conf);
+
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);        /*flag used to allocate isr*/
+
+    gpio_isr_handler_add(sensor , isr_handler ,(void*)sensor);
+
+     int cnt = 0;
+     while(1)
+     {
+        printf("cnt : %d \n ",cnt++);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        gpio_set_level(sensor , cnt % 2);
+     }
+    
+      
 }
