a701dd398b4e12126d6b1532ac1565122564816d
Farhana-Jamal <farhanajamaludeen@gmail.com>
2022-11-25 09:26:10 +0530

    read Acc

--- src/main.c
+++ src/main.c
@@ -14,7 +14,12 @@
 #define mpuPwrMngmnt 0x6B
 #define mpuConfig 0x1A
 #define mpuAccConfig 0x1C 
-#define mpuAcc 0x3B
+#define mpuAccXH 0x3B
+#define mpuAccXL 0x3C
+#define mpuAccYH 0x3D
+#define mpuAccYL 0x3E
+#define mpuAccZH 0x3F
+#define mpuAccZL 0x50
 #define mpuGyroConfig 0x1B
 #define mpuGyro 0x43
 #define mpuTemp 0x41
@@ -66,25 +71,19 @@ void mpu6050Init()
     i2c_master_write_byte(cmd,(mpuAddress << 1) | I2C_MASTER_WRITE, true);
     i2c_master_write_byte(cmd , mpuPwrMngmnt , true);
     i2c_master_write_byte(cmd , 0x0 , true);
-    i2c_master_stop(cmd);
-    i2c_master_write_byte(cmd,(mpuAddress << 1) | I2C_MASTER_WRITE, true);
     i2c_master_write_byte(cmd , mpuGyroConfig ,true);
     i2c_master_write_byte(cmd , 0x00000000 ,true);
-    i2c_master_stop(cmd);
-    i2c_master_write_byte(cmd,(mpuAddress << 1) | I2C_MASTER_WRITE, true);
     i2c_master_write_byte(cmd , mpuAccConfig , true);
     i2c_master_write_byte(cmd ,0b00000000 ,true);
     i2c_master_stop(cmd);
 
     espRC = i2c_master_cmd_begin(I2C_NUM_0 ,cmd ,10/portTICK_PERIOD_MS);
-    if (espRC == ESP_OK)
+    if (espRC == ESP_OK) 
     {
-        printf(" mpu initialized");
-    }
-    else
-    {
-        ESP_LOGI(TAG , "MPU is not connected");
-    }
+		ESP_LOGI(TAG, "mpu configured successfully");
+	} else {
+		ESP_LOGE(TAG, "mpu configuration failed. code: 0x%.2X", espRC);
+	}
     i2c_cmd_link_delete(cmd);
 
 }
@@ -94,31 +93,28 @@ void readAcc()
     esp_err_t espRC;
     i2c_cmd_handle_t cmd;
 
+    uint8_t AccXH , AccXL ,AccYH , AccYL , AccZH , AccZL;
+
     while(1)
     {
 
     cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd , (mpuAddress <<1)|I2C_MASTER_WRITE , true);
-    i2c_master_write_byte(cmd , mpuAcc , true);
+    i2c_master_write_byte(cmd , mpuAccXH , true);
+    i2c_master_write_byte(cmd , mpuAccXL , true);
     i2c_master_stop(cmd);
 
-    uint8_t AccXH ,AccXL , AccYH ,AccYL , AccZH ,AccZL ;
-
+    cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
-    i2c_master_write_byte(cmd , (mpuAddress <<1)|I2C_MASTER_WRITE , true);
+    i2c_master_write_byte(cmd , (mpuAddress <<1)|I2C_MASTER_READ , true);
     i2c_master_read_byte(cmd , &AccXH , ACK_VAL);
-    i2c_master_read_byte(cmd , &AccXL , ACK_VAL);
-    i2c_master_read_byte(cmd , &AccYH , ACK_VAL);
-    i2c_master_read_byte(cmd , &AccYL , ACK_VAL);
-    i2c_master_read_byte(cmd , &AccZH , ACK_VAL);
-    i2c_master_read_byte(cmd , &AccZL ,NACK_VAL);
+    i2c_master_read_byte(cmd , &AccXL, ACK_VAL);
     i2c_master_stop(cmd);
-
     espRC = i2c_master_cmd_begin(I2C_NUM_0 , cmd , 10/portTICK_PERIOD_MS);
     if(espRC == ESP_OK)
     {
-        ESP_LOGI(TAG , " got Accln values");
+        ESP_LOGI(TAG , " got AcclnX values");
     }
     else
     {
@@ -127,16 +123,70 @@ void readAcc()
 
     i2c_cmd_link_delete(cmd);
 
-    int16_t AccX , AccY , AccZ;
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd , (mpuAddress <<1)|I2C_MASTER_WRITE , true);
+    i2c_master_write_byte(cmd , mpuAccYH , true);
+    i2c_master_write_byte(cmd , mpuAccYL , true);
+    i2c_master_stop(cmd);
+
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd , (mpuAddress <<1)|I2C_MASTER_READ , true);
+    i2c_master_read_byte(cmd , &AccYH , ACK_VAL);
+    i2c_master_read_byte(cmd , &AccYL , ACK_VAL);
+    i2c_master_stop(cmd);
+    espRC = i2c_master_cmd_begin(I2C_NUM_0 , cmd , 10/portTICK_PERIOD_MS);
+    if(espRC == ESP_OK)
+    {
+        ESP_LOGI(TAG , " got AcclnY values");
+    }
+    else
+    {
+        ESP_LOGI(TAG , "FAiled (accltn)");
+    }
+
+    i2c_cmd_link_delete(cmd);
+
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd , (mpuAddress <<1)|I2C_MASTER_WRITE , true);
+    i2c_master_write_byte(cmd , mpuAccZH , true);
+    i2c_master_write_byte(cmd , mpuAccZL , true);
+    i2c_master_stop(cmd);
+
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd , (mpuAddress <<1)|I2C_MASTER_READ , true);
+
+    i2c_master_read_byte(cmd , &AccZH , ACK_VAL);
+    i2c_master_read_byte(cmd , &AccZL , NACK_VAL);
+    i2c_master_stop(cmd);
+
+    espRC = i2c_master_cmd_begin(I2C_NUM_0 , cmd , 10/portTICK_PERIOD_MS);
+    if(espRC == ESP_OK)
+    {
+        ESP_LOGI(TAG , " got AcclnZ values");
+    }
+    else
+    {
+        ESP_LOGI(TAG , "FAiled (accltn)");
+    }
+
+    i2c_cmd_link_delete(cmd);
+
+    float AccX , AccY , AccZ;
 
     AccX = (AccXH << 8 | AccXL);
     AccY = (AccYH << 8 | AccYL);
     AccZ = (AccZH << 8 | AccZL);
 
     esp_log_level_set ("ACC" , ESP_LOG_INFO);
-    ESP_LOGI("ACC" , "AccX : %d" , AccX);
-    ESP_LOGI("ACC" , "AccY : %d" , AccY);
-    ESP_LOGI("ACC" , "AccZ : %d" , AccZ);
+    ESP_LOGI("ACC" , "AccX : %f" , AccX);
+    ESP_LOGI("ACC" , "AccY : %f" , AccY);
+    ESP_LOGI("ACC" , "AccZ : %f" , AccZ);
+
+    vTaskDelay(1000/portTICK_PERIOD_MS);
     }
 
 }
@@ -145,8 +195,7 @@ void app_main()
 {
    masterEsp32Init();
    mpu6050Init();
-
-//    xTaskCreate(&readAcc , "accclrtn task" , 2048 , NULL , 5 , NULL);
+   xTaskCreate(&readAcc , "accclrtn task" , 2048 , NULL , 5 , NULL);
    
 }
 
