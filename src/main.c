6dfba866d4822d3c25227fb7b349cefe83e1e313
Farhana-Jamal <farhanajamaludeen@gmail.com>
2022-11-26 22:59:52 +0530

    Read Accln ,gyro and temp

--- src/main.c
+++ src/main.c
@@ -9,37 +9,17 @@
 #define SCL_PIN GPIO_NUM_22
 
 #define mpuAddress 0x68
-#define mpuSmplRate 0x19
-#define whoAmI 0x75
 #define mpuPwrMngmnt 0x6B
-#define mpuConfig 0x1A
-
-#define mpuAccConfig 0x1C 
-#define mpuAccXH 0x3B
-#define mpuAccXL 0x3C
-#define mpuAccYH 0x3D
-#define mpuAccYL 0x3E
-#define mpuAccZH 0x3F
-#define mpuAccZL 0x50
-
-#define mpuGyroConfig 0x1B
-#define mpuGyroXH 0x43
-#define mpuGyroXL 0x44
-#define mpuGyroYH 0x45
-#define mpuGyroYL 0x46
-#define mpuGyroZH 0x47
-#define mpuGyroZL 0x48
-
+#define mpuAccX 0x3B
+#define mpuGyroX 0x43
 #define mpuTemp 0x41
 
 #define ACCEL_TRANSFORMATION_NUMBER     0.00006103515 // (1 / 16384) precalculated
-// #define GYRO_TRANSFORMATION_NUMBER      131.0 // (131.0) precalculated
+#define GYRO_TRANSFORMATION_NUMBER      131.0 // (131.0) precalculated
 
 #define ACK_VAL    0x0
 #define NACK_VAL   0x1
 
-#define PCA9548ADDR   0x70
-#define  PCA9548_CH1  0x01
 
 
 #define TAG "MPU6050"
@@ -57,12 +37,11 @@ void masterEsp32Init(void)
         .master.clk_speed = 1000000,
         .clk_flags = 0
     };
-//     i2c_param_config(I2C_NUM_0, &i2c_config);
-//     i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
-       ESP_ERROR_CHECK(i2c_param_config(I2C_NUM_0, &i2c_config));
-       printf("- i2c controller configured\r\n");
 
-    // install the driver
+    ESP_ERROR_CHECK(i2c_param_config(I2C_NUM_0, &i2c_config));
+    printf("- i2c controller configured\r\n");
+
+    //  install the driver
     ESP_ERROR_CHECK(i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0));
     printf("- i2c driver installed\r\n\r\n");
 }
@@ -79,10 +58,6 @@ void mpu6050Init()
     i2c_master_write_byte(cmd,(mpuAddress << 1) | I2C_MASTER_WRITE, true);
     i2c_master_write_byte(cmd , mpuPwrMngmnt , true);
     i2c_master_write_byte(cmd , 0x0 , true);
-    i2c_master_write_byte(cmd , mpuGyroConfig ,true);
-    i2c_master_write_byte(cmd , 0x00000000 ,true);
-    i2c_master_write_byte(cmd , mpuAccConfig , true);
-    i2c_master_write_byte(cmd ,0b00000000 ,true);
     i2c_master_stop(cmd);
 
     espRC = i2c_master_cmd_begin(I2C_NUM_0 ,cmd ,10/portTICK_PERIOD_MS);
@@ -98,10 +73,14 @@ void mpu6050Init()
 
 }
 
+
+
 void readAcc()
 {
     esp_err_t espRC;
     i2c_cmd_handle_t cmd;
+
+    uint8_t AccXH , AccXL ,AccYH ,AccYL ,AccZH ,AccZL ;
 
     while(1)
     {
@@ -109,28 +88,21 @@ void readAcc()
     cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd , (mpuAddress <<1)|I2C_MASTER_WRITE , true);
-    i2c_master_write_byte(cmd , mpuAccXH , true);
-    i2c_master_write_byte(cmd , mpuAccXL , true);
-    i2c_master_write_byte(cmd , mpuAccYH , true);
-    i2c_master_write_byte(cmd , mpuAccYL , true);
-    i2c_master_write_byte(cmd , mpuAccZH , true);
-    i2c_master_write_byte(cmd , mpuAccZL , true);
-    i2c_master_stop(cmd);
-
-    i2c_master_cmd_begin(I2C_NUM_0 , cmd , 10/portTICK_PERIOD_MS);
-    i2c_cmd_link_delete(cmd);
-
-    uint8_t AccXH ,AccXL , AccYH ,AccYL , AccZH ,AccZL ;
-
-    cmd = i2c_cmd_link_create();
+    i2c_master_write_byte(cmd , mpuAccX , true);
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd , (mpuAddress <<1)|I2C_MASTER_READ , true);
-    i2c_master_read_byte(cmd , &AccXH , ACK_VAL);
-    i2c_master_read_byte(cmd , &AccXL , ACK_VAL);
-    i2c_master_read_byte(cmd , &AccYH , ACK_VAL);
-    i2c_master_read_byte(cmd , &AccYL , ACK_VAL);
-    i2c_master_read_byte(cmd , &AccZH , ACK_VAL);
-    i2c_master_read_byte(cmd , &AccZL ,NACK_VAL);
+
+    // if(size > 1)
+    // {
+    //     i2c_master_read(cmd , data ,size -1 , ACK_VAL);
+      
+    // }
+    i2c_master_read_byte(cmd ,&AccXH , ACK_VAL );
+    i2c_master_read_byte(cmd ,&AccXL , ACK_VAL );
+    i2c_master_read_byte(cmd ,&AccYH , ACK_VAL ); 
+    i2c_master_read_byte(cmd ,&AccYL , ACK_VAL ); 
+    i2c_master_read_byte(cmd ,&AccZH , ACK_VAL);
+    i2c_master_read_byte(cmd , &AccZL, NACK_VAL);
     i2c_master_stop(cmd);
 
     espRC = i2c_master_cmd_begin(I2C_NUM_0 , cmd , 10/portTICK_PERIOD_MS);
@@ -145,90 +117,153 @@ void readAcc()
 
     i2c_cmd_link_delete(cmd);
 
-    int16_t AccX , AccY , AccZ;
+    int16_t AccRawX , AccRawY , AccRawZ;
+    float AccX , AccY , AccZ;
 
-    AccX = (AccXH << 8 | AccXL);
-    AccY = (AccYH << 8 | AccYL);
-    AccZ = (AccZH << 8 | AccZL);
+    AccRawX = (AccXH << 8 | AccXL);
+    AccRawY = (AccYH << 8 | AccYL);
+    AccRawZ = (AccZH << 8 | AccZL);
+
+    AccX = AccRawX * ACCEL_TRANSFORMATION_NUMBER ;
+    AccY = AccRawY * ACCEL_TRANSFORMATION_NUMBER ;
+    AccZ = AccRawZ * ACCEL_TRANSFORMATION_NUMBER ;
+
 
     esp_log_level_set ("ACC" , ESP_LOG_INFO);
-    ESP_LOGI("ACC" , "AccX : %d" , AccX);
-    ESP_LOGI("ACC" , "AccY : %d" , AccY);
-    ESP_LOGI("ACC" , "AccZ : %d" , AccZ);
+    ESP_LOGI("ACC" , "AccX : %f" , AccX);
+    ESP_LOGI("ACC" , "AccY : %f" , AccY);
+    ESP_LOGI("ACC" , "AccZ : %f" , AccZ);
+
+   
 
     vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 
 }
 
+
 void readGyro()
 {
-    esp_err_t espRC ;
-    i2c_cmd_handle_t cmd ;
+    esp_err_t espRC;
+    i2c_cmd_handle_t cmd;
+
+    uint8_t GyroXH , GyroXL ,GyroYH ,GyroYL ,GyroZH ,GyroZL ;
+
 
     while(1)
     {
-        cmd = i2c_cmd_link_create();
-        i2c_master_start(cmd);
-        i2c_master_write_byte(cmd , (mpuAddress << 1) | I2C_MASTER_WRITE , true);
-        i2c_master_write_byte(cmd , mpuGyroXH , true);
-        i2c_master_write_byte(cmd , mpuGyroXL , true);
-        i2c_master_write_byte(cmd , mpuGyroYH , true);
-        i2c_master_write_byte(cmd , mpuGyroYL , true);
-        i2c_master_write_byte(cmd , mpuGyroZH , true);
-        i2c_master_write_byte(cmd , mpuGyroZL , true);
-        // i2c_master_stop(cmd);
 
-        // ESP_ERROR_CHECK(i2c_master_cmd_begin(I2C_NUM_0 , cmd , 10/portTICK_PERIOD_MS));
-        // printf("gyro configured \n");
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd , (mpuAddress <<1)|I2C_MASTER_WRITE , true);
+    i2c_master_write_byte(cmd , mpuGyroX , true);
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd , (mpuAddress <<1)|I2C_MASTER_READ , true);
 
-        // i2c_cmd_link_delete(cmd);
+    
+    i2c_master_read_byte(cmd ,&GyroXH , ACK_VAL );
+    i2c_master_read_byte(cmd ,&GyroXL , ACK_VAL );
+    i2c_master_read_byte(cmd ,&GyroYH , ACK_VAL ); 
+    i2c_master_read_byte(cmd ,&GyroYL , ACK_VAL ); 
+    i2c_master_read_byte(cmd ,&GyroZH , ACK_VAL);
+    i2c_master_read_byte(cmd ,&GyroZL, NACK_VAL);
 
-        uint8_t GyroXH , GyroXL , GyroYH , GyroYL , GyroZH ,GyroZL ;
+    i2c_master_stop(cmd);
 
-        // cmd = i2c_cmd_link_create();
-        i2c_master_start(cmd);
-        i2c_master_write_byte(cmd , (mpuAddress << 1) | I2C_MASTER_READ , true);
-        i2c_master_read_byte(cmd , &GyroXH , ACK_VAL);
-        i2c_master_read_byte(cmd , &GyroXL , ACK_VAL);
-        i2c_master_read_byte(cmd , &GyroYH , ACK_VAL);
-        i2c_master_read_byte(cmd , &GyroYL , ACK_VAL);
-        i2c_master_read_byte(cmd , &GyroZH , ACK_VAL);
-        i2c_master_read_byte(cmd , &GyroZL , NACK_VAL);
-        i2c_master_stop(cmd);
+    espRC = i2c_master_cmd_begin(I2C_NUM_0 , cmd , 10/portTICK_PERIOD_MS);
+    if(espRC == ESP_OK)
+    {
+        ESP_LOGI(TAG , " got gyro values");
+    }
+    else
+    {
+        ESP_LOGI(TAG , "FAiled (gyro)");
+    }
 
-        espRC = i2c_master_cmd_begin(I2C_NUM_0 , cmd , 10/portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
 
-        if(espRC == ESP_OK)
-        {
-            ESP_LOGI(TAG , "got gyro vaalues" );
-        }
-        else
-        {
-            ESP_LOGI(TAG , "failed (gyro)");
-        }
+   
 
-        i2c_cmd_link_delete(cmd);
+    int16_t gyroRawX , gyroRawY , gyroRawZ; 
+    float GYROX , GYROY ,GYROZ ;
 
-        int16_t GYROX , GYROY ,GYROZ ;
+    gyroRawX = (GyroXH << 8 | GyroXL);
+    gyroRawY = (GyroYH << 8 | GyroYL);
+    gyroRawZ = (GyroZH << 8 | GyroZL);
 
-        GYROX = (GyroXH << 8 | GyroXL);
-        GYROY = (GyroYH << 8 | GyroYL);
-        GYROZ = (GyroZH << 8 | GyroZL);
+    GYROX = gyroRawX * GYRO_TRANSFORMATION_NUMBER ;
+    GYROY = gyroRawY * GYRO_TRANSFORMATION_NUMBER ;
+    GYROZ = gyroRawZ * GYRO_TRANSFORMATION_NUMBER ;
 
-        esp_log_level_set ("GYRO" , ESP_LOG_INFO);
-        ESP_LOGI("GYRO" , "GyroX : %d" , GYROX);
-        ESP_LOGI("GYRO" , "GyroY : %d" , GYROY);
-        ESP_LOGI("GYRO" , "GyroZ : %d" , GYROZ);
+    // GYROX = (data[6] << 8 | data[7]);
+    // GYROY = (data[8] << 8 | data[9]);
+    // GYROZ = (data[10] << 8 | data[11]);
 
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    esp_log_level_set ("GYRO" , ESP_LOG_INFO);
+    ESP_LOGI("GYRO" , "GyroX : %f" , GYROX);
+    ESP_LOGI("GYRO" , "GyroY : %f" , GYROY);
+    ESP_LOGI("GYRO" , "GyroZ : %f" , GYROZ);
 
-
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
 
 
     }
 
 }
+
+void readTemp()
+{
+    esp_err_t espRC;
+    i2c_cmd_handle_t cmd;
+
+    uint8_t TempH , TempL  ;
+
+
+    while(1)
+    {
+
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd , (mpuAddress <<1)|I2C_MASTER_WRITE , true);
+    i2c_master_write_byte(cmd , mpuTemp , true);
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd , (mpuAddress <<1)|I2C_MASTER_READ , true);
+
+    i2c_master_read_byte(cmd ,&TempH , ACK_VAL );
+    i2c_master_read_byte(cmd ,&TempL , NACK_VAL );
+    
+    i2c_master_stop(cmd);
+
+    espRC = i2c_master_cmd_begin(I2C_NUM_0 , cmd , 10/portTICK_PERIOD_MS);
+    if(espRC == ESP_OK)
+    {
+        ESP_LOGI(TAG , " got temp values");
+    }
+    else
+    {
+        ESP_LOGI(TAG , "FAiled (temp)");
+    }
+
+    i2c_cmd_link_delete(cmd);
+
+   
+
+    int16_t tempRaw ;
+    float Temp ;
+
+    tempRaw = (TempH << 8 | TempL);
+
+    Temp = tempRaw / 340 + 36.53  ;
+    
+    esp_log_level_set ("TEMP" , ESP_LOG_INFO);
+    ESP_LOGI("TEMP" , "TEMP : %f" , Temp);
+    
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+}
+
 
 void app_main()
 {
@@ -236,8 +271,13 @@ void app_main()
    mpu6050Init();
 
    xTaskCreate(&readAcc , "accclrtn task" , 2048 , NULL , 5 , NULL);
-   vTaskDelay(1000 / portTICK_PERIOD_MS);
+   
    
    xTaskCreate(&readGyro , "GYRO task " , 2048 , NULL , 5 , NULL);
+
+   xTaskCreate(&readTemp , " TEmp task " , 2048 , NULL , 5 , NULL);
+   vTaskDelay(1000 / portTICK_PERIOD_MS);
 }
+
+
 
